dc0ac586ade4c731bdc3cf483018ad66392ec7ee
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 15:53:59 -0500

    moving to server action

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -2,10 +2,15 @@
 
 import {signOut} from "next-auth/react";
 import {useEffect} from "react";
+import { logout } from "../actions/logout-action";
 
 export default function Page() {
     useEffect(() => {
-        window.location.href = `https://next-auth-okta-chi.vercel.app/auth/signout`;
+         // window.location.href = `https://next-auth-okta-chi.vercel.app/auth/signout`;
+         async function callLogOutAction() {
+            await logout();
+         }
+         callLogOutAction();         
     }, []);
 
     return <p>Logging out...</p>;
