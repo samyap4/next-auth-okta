56987231fd9a8c21ac45483ac0a42fe3da57b209
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 12:40:23 -0500

    implementing the refresh and the rotation

--- auth.ts
+++ auth.ts
@@ -27,12 +27,40 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       if (pathname === "/middleware-example") return !!auth
       return true
     },
-    jwt({ token, trigger, session, account }) {
-      if (trigger === "update") token.name = session.user.name
-      if (account?.provider === "keycloak") {
-        return { ...token, accessToken: account.access_token }
+    async jwt({ token, account }: any) {
+      if (account) {
+          token.accessToken = account.access_token;
+          token.idToken = account.id_token;
+          token.oktaId = account.providerAccountId;
+          token.refreshToken = account.refresh_token;
       }
-      return token
+    
+      var tokenParsed = JSON.parse(Buffer.from(token.idToken.split('.')[1], 'base64').toString());
+      const dateNowInSeconds = new Date().getTime() / 1000
+      const tokenIsNotExpired = dateNowInSeconds < tokenParsed.exp;
+      if (tokenIsNotExpired) {
+          return token;
+      } else {
+          const url = `${process.env.OKTA_DOMAIN}/v1/token`;
+          const body = `grant_type=refresh_token&client_id=${process.env.OKTA_CLIENT_ID}&client_secret=${process.env.OKTA_CLIENT_SECRET}&refresh_token=${token.refreshToken}`;
+          const headers = {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Accept': 'application/json'
+          };
+          const response = await fetch(url, { method: 'POST', body, headers });
+          const data = await response.json();
+          if (data.error) {
+              throw new Error("Unable to refresh token");
+          }
+
+          // Only need to refresh id/access token with new refresh token not oktaId since it's not returned
+          if (data && data.success) {
+              token.accessToken = data.access_token;
+              token.idToken = data.id_token;
+              token.refreshToken = data.refresh_token;
+          }
+          return token;
+      }
     },
     async session({ session, token }) {
       if (token?.accessToken) session.accessToken = token.accessToken
