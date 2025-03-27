d8e22a576139bc0c481747d8725cd33e5acdf585
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 13:38:39 -0500

    Update route.ts

--- app/api/auth/federated-logout/route.ts
+++ app/api/auth/federated-logout/route.ts
@@ -3,18 +3,11 @@ import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    
-    if (req.method === 'POST') {
-        try {
-            const token = await getToken({ req });
-            // if (!token) {
-            //   return res.redirect(process.env.NEXTAUTH_URL || '')
-            // }
-            
-            const endsessionURL = `https://${process.env.OKTA_ISSUER}/v1/logout?id_token_hint=${token?.idToken}&post_logout_redirect_uri=${process.env.NEXT_AUTH_LOGOUT_ROUTE}`
-            return NextResponse.redirect(`${endsessionURL}`)
-          } catch (error) {
-            // res.redirect(process.env.NEXTAUTH_URL)
-          }
-        }
+    try {
+        const token = await getToken({ req });
+        const endsessionURL = `https://${process.env.OKTA_ISSUER}/v1/logout?id_token_hint=${token?.idToken}&post_logout_redirect_uri=${process.env.NEXT_AUTH_LOGOUT_ROUTE}`
+        NextResponse.redirect(`${endsessionURL}`)
+    } catch (error) {
+        NextResponse.redirect(`${process.env.NEXT_AUTH_LOGOUT_ROUTE}`)
+    }
 }
