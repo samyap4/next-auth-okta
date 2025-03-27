cdf9a7ee2c06d283a5b94c8cc0be8a2cf32e1f38
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 16:24:35 -0500

    client side log out func

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -3,17 +3,19 @@
 import { useEffect } from "react";
 import { logout } from "../actions/logout-action";
 import { useRouter } from 'next/navigation'
+import { signIn, signOut } from "next-auth/react"
 
 export default function Page() {
     const router = useRouter();
 
     useEffect(() => {
-        async function callLogOutAction() {
-            const result = await logout();
-            console.log(result);
-            window.location.href = 'https://next-auth-okta-chi.vercel.app/'
-        }
-        callLogOutAction();
+        // async function callLogOutAction() {
+        //     const result = await logout();
+        //     console.log(result);
+        //     window.location.href = 'https://next-auth-okta-chi.vercel.app/'
+        // }
+        // callLogOutAction();
+        signOut({redirectTo: '/'});
     }, []);
 
     return <p>Logging out...</p>;
