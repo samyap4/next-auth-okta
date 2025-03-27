b03b97b3969ff8a6fb8e5fd52f009e06ccefeac4
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 14:38:28 -0500

    lets see

--- app/api/auth/federated-logout/route.ts
+++ app/api/auth/federated-logout/route.ts
@@ -1,12 +1,11 @@
-
-import { getToken } from "next-auth/jwt"
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
+import { auth } from "@/auth";
 
 async function handler(req: NextRequest) {
     try {
-        const token = await getToken({ req });
-        const endsessionURL = `https://${process.env.OKTA_ISSUER}/v1/logout?id_token_hint=${token?.idToken}&post_logout_redirect_uri=${process.env.NEXT_AUTH_LOGOUT_ROUTE}`
+        const session = await auth() as any;
+        const endsessionURL = `https://${process.env.OKTA_ISSUER}/v1/logout?id_token_hint=${session?.idToken}&post_logout_redirect_uri=${process.env.NEXT_AUTH_LOGOUT_ROUTE}`
         return redirect(`${endsessionURL}`)
     } catch (error) {
         return redirect(`${process.env.NEXT_AUTH_LOGOUT_ROUTE}`)
