44e018ff4f470b1264b2f40ee43567fcd9de2184
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 13:21:34 -0500

    implementing federated logout

---
+++ app/api/auth/federated-logout/route.ts
@@ -0,0 +1,20 @@
+
+import { getToken } from "next-auth/jwt"
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function federatedLogout(req: NextRequest, res: NextResponse) {
+    
+    if (req.method === 'POST') {
+        try {
+            const token = await getToken({ req });
+            // if (!token) {
+            //   return res.redirect(process.env.NEXTAUTH_URL || '')
+            // }
+            
+            const endsessionURL = `https://${process.env.OKTA_ISSUER}/v1/logout?id_token_hint=${token?.idToken}&post_logout_redirect_uri=${process.env.NEXT_AUTH_LOGOUT_ROUTE}`
+            return NextResponse.redirect(`${endsessionURL}`)
+          } catch (error) {
+            // res.redirect(process.env.NEXTAUTH_URL)
+          }
+        }
+}
--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,5 +1,6 @@
 import { signIn, signOut } from "auth"
 import { Button } from "./ui/button"
+import router from "next/router"
 
 export function SignIn({
   provider,
@@ -19,16 +20,24 @@ export function SignIn({
 
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signOut()
-      }}
-      className="w-full"
-    >
-      <Button variant="ghost" className="w-full p-0" {...props}>
+    // <form
+    //   action={async () => {
+    //     "use server"
+    //     await signOut()
+    //   }}
+    //   className="w-full"
+    // >
+    //   <Button variant="ghost" className="w-full p-0" {...props}>
+    //     Sign Out
+    //   </Button>
+    // </form>
+
+      <Button variant="ghost" className="w-full p-0" {...props} onClick={signOutHandler}>
         Sign Out
       </Button>
-    </form>
   )
+}
+
+function signOutHandler() {
+  router.push("/auth/federated-sign-out");
 }
