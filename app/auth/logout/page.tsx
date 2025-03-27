a40034cd33263ba27ef79d672007356ff91da4ee
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 15:42:48 -0500

    Update page.tsx

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -5,7 +5,7 @@ import {useEffect} from "react";
 
 export default function Page() {
     useEffect(() => {
-        window.location.href = `https://next-auth-okta-chi.vercel.app/api/auth/signout`;
+        window.location.href = `https://next-auth-okta-chi.vercel.app/auth/signout`;
     }, []);
 
     return <p>Logging out...</p>;
