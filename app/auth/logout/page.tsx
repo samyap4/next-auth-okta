40d15290516bbd5a7e1cd326c6be02ef2b7e6aa7
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 16:29:19 -0500

    moving navigation to server action

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -9,14 +9,10 @@ export default function Page() {
     const router = useRouter();
 
     useEffect(() => {
-        // async function callLogOutAction() {
-        //     const result = await logout();
-        //     console.log(result);
-        //     window.location.href = 'https://next-auth-okta-chi.vercel.app/'
-        // }
-        // callLogOutAction();
-        signOut({redirect: false});
-        router.push('/')
+        async function callLogOutAction() {
+            await logout();
+        }
+        callLogOutAction();
     }, []);
 
     return <p>Logging out...</p>;
