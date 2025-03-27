29695369a0997d0b2155b59b39924d7375bf4bdd
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-03-27 16:04:47 -0500

    Update page.tsx

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
             await logout();
         }
         callLogOutAction();
-         
+        setInterval(() => console.log('//HACK: waiting for server side logout to complete', 300)); 
         window.location.href = 'https://next-auth-okta-chi.vercel.app/'
     }, []);
 
